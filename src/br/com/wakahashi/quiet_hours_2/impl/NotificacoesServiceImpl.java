9d44bf6bc56136248810cdf13411433d2db7dc61
Leonardo Wada <leonardowada@hotmail.com>
2014-11-14 18:37:19 -0200

    teste3 gerrithub
    Change-Id: Idbced8b1fb7d414094238eefcf7925c88a6ae6ed

--- src/br/com/wakahashi/quiet_hours_2/impl/NotificacoesServiceImpl.java
+++ src/br/com/wakahashi/quiet_hours_2/impl/NotificacoesServiceImpl.java
@@ -29,7 +29,7 @@ public class NotificacoesServiceImpl implements NotificacoesService {
 
 	@Override
 	public void silenciarFlash() {
-		//TEste para review do gerrit
+		//TEste gerrithub
 		// TODO Auto-generated method stub
 
 	}
